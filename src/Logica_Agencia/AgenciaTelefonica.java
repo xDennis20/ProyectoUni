00c2d9f14d76d6ba1a0aa36b02d1a348c0574b24
xDennis20 <dennis_6@outlook.com>
2025-06-18 11:40:29 -0500

    Agregue el metodo buscar contactos y mostrar contactos del diccionario

--- src/Logica_Agencia/AgenciaTelefonica.java
+++ src/Logica_Agencia/AgenciaTelefonica.java
@@ -8,4 +8,13 @@ public class AgenciaTelefonica {
     public void agregarContacto(String nombre, String telefono) {
         contactos.put(nombre, new Contacto(nombre, telefono)); //put() añade un par clave-valor al HashMap.
     }
+    public Contacto buscarContacto(String nombre) {
+        return contactos.get(nombre);  // Busca el contacto en el diccionario y si no esta devuelve valor Null (None)
+    }
+
+    public void mostrarContactos(){
+        contactos.entrySet().stream() // Convierte el HashMap en una lista de pares [clave, valor].
+                .sorted(Map.Entry.comparingByKey()) // stream() + sorted(): Ordena los contactos alfabéticamente por nombre (clave)
+                .forEach(entry -> System.out.println(entry.getValue())); // forEach(): Imprime cada contacto.
+    }
 }
